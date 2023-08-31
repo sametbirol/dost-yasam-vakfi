57bfbb75502b724aa934657479c5ebe53987a87a
sametbirol <sametbirol34@gmail.com>
2023-08-31 13:43:30 +0300

    footer message

--- src/layouts/Footer.vue
+++ src/layouts/Footer.vue
@@ -1,10 +1,16 @@
 <template>
   <div id="footer">
     <footer>
-      <div style="background-color: rgba(0, 0, 0, 0.2)">
+      <div>
         © 2022 Copyright
       </div>
+      <p>
+      Bu sitenin içeriği ziyaretçilerini bilgilendirmeye 
+      yönelik hazırlanmış olup tıbbi teşhis, tedavi veya reçete bilgisi özelliği taşımaz.
+    </p>
+      
     </footer>
+    
   </div>
 </template>
   
@@ -20,8 +26,13 @@
 footer {
   position: sticky;
   width: 100%;
+  background-color: rgba(0, 0, 0, 0.2);
+  display: flex;
+  justify-content: space-around;
 }
 #footer{
-  width:100%
+  width:100%;
+
+
 }
 </style>
