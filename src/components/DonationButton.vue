9e5456a5e5f75ad76eac6436ac4126bb57d219b7
sametbirol <sametbirol34@gmail.com>
2023-09-02 13:37:06 +0300

    color and background propped button

---
+++ src/components/DonationButton.vue
@@ -0,0 +1,30 @@
+<template>
+    <button class="btn"><slot>Donate Now</slot></button>
+</template>
+<script setup>
+const props = defineProps({
+    color : {
+        type: String,
+        default: "black"
+    },
+    bgcolor : {
+        type: String,
+        default: "white"
+    }
+})
+</script>
+<style scoped>
+.btn{
+    color:v-bind("props.color");
+    background-color: v-bind("props.bgcolor");
+    padding: 16px;
+    border-radius: 32px;
+    font-size: 24px;
+    font-family:Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif   ;
+    letter-spacing: 1px;
+    
+}
+.btn:hover{
+    cursor: pointer;
+}
+</style>
