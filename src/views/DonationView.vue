5024f2ba98b02c89b244cf3fa25ed35af8593197
sametbirol <sametbirol34@gmail.com>
2023-09-03 03:08:56 +0300

    Banks added

--- src/views/DonationView.vue
+++ src/views/DonationView.vue
@@ -1,10 +1,12 @@
 <template>
     <DonationHero></DonationHero>
     <DonationHero2></DonationHero2>
+    <DonationBanks></DonationBanks>
 </template>
 <script setup>
 import DonationHero from '../components/DonationHero.vue';
 import DonationHero2 from '../components/DonationHero2.vue';
+import DonationBanks from '../components/DonationBanks.vue';
 </script>
 <style scoped>
 </style>
