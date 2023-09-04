d4b0f7abd11b649aa4d07404bb76931a95c48735
sametbirol <sametbirol34@gmail.com>
2023-09-04 20:24:11 +0300

    remove redundant lines

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,27 +1,15 @@
 <template>
-    <!-- <MediaHolder :image="`url(${image})`">
-        <template v-slot:header>
-            <h1 class="title">Dost Yaşam Vakfı Kimdir?</h1>
-        </template>
-        <template v-slot:router>
-            <RouterLink to="/" class="inline">Anasayfa</RouterLink> / 
-            <RouterLink to="/hakkimizda" class="inline">Hakkımızda</RouterLink> /
-            <a class="disabled">Dost Yaşam Vakfı Kimdir?</a>
-        </template>
-
-    </MediaHolder> -->
     <AboutUsHero></AboutUsHero>
     <AboutUsHero2></AboutUsHero2>
     <AboutUsFounders></AboutUsFounders>
     <AboutUsActivities></AboutUsActivities>
-    <div>Hakkımızda</div>
+    <AboutUsFAQ></AboutUsFAQ>
+    ,
 </template>
 <script setup>
 import AboutUsHero from '../components/AboutUs/AboutUsHero.vue';
 import AboutUsHero2 from '../components/AboutUs/AboutUsHero2.vue';
 import AboutUsFounders from '../components/AboutUs/AboutUsFounders.vue';
 import AboutUsActivities from '../components/AboutUs/AboutUsActivities.vue';
-import MediaHolder from '@/components/MediaHolder.vue'
-import image from '@/assets/media/default-baby.jpg'
-import { RouterLink } from 'vue-router';
+import AboutUsFAQ from '../components/AboutUs/AboutUsFAQ.vue';
 </script>
