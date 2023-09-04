8bc33012cf12a53de0068854e40a537d97c1b6b8
sametbirol <sametbirol34@gmail.com>
2023-09-04 18:06:52 +0300

    adjust path

--- src/views/DonationView.vue
+++ src/views/DonationView.vue
@@ -4,9 +4,9 @@
     <DonationBanks></DonationBanks>
 </template>
 <script setup>
-import DonationHero from '../components/DonationHero.vue';
-import DonationHero2 from '../components/DonationHero2.vue';
-import DonationBanks from '../components/DonationBanks.vue';
+import DonationHero from '../components/Donation/DonationHero.vue';
+import DonationHero2 from '../components/Donation/DonationHero2.vue';
+import DonationBanks from '../components/Donation/DonationBanks.vue';
 </script>
 <style scoped>
 </style>
