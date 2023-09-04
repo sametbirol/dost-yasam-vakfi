2e18c805805470bbd3342f6b4c70af3271662578
sametbirol <sametbirol34@gmail.com>
2023-09-04 20:24:25 +0300

    scroll up button for future usage

---
+++ src/components/func/ScrollUp.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="circle">
+        <button class="upward" @click="scrollToTopofPage"><img src="../../assets/media/upwardArrow.svg" width="64" height="64"/></button>
+    </div>
+</template>
+<script setup>
+const scrollToTopofPage = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
+</script>
+<style scoped>
+.upward{
+    position: absolute;
+    right: 30px;
+    bottom: 30px;
+}
+</style>
