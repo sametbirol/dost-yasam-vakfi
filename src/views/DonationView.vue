28e69cbce22f535bcd48f280b430d8abcf2227f8
sametbirol <sametbirol34@gmail.com>
2023-09-06 16:42:12 +0300

    remove bank section

--- src/views/DonationView.vue
+++ src/views/DonationView.vue
@@ -1,12 +1,12 @@
 <template>
     <DonationHero></DonationHero>
     <DonationHero2></DonationHero2>
-    <DonationBanks></DonationBanks>
+    <!-- <DonationBanks></DonationBanks> -->
 </template>
 <script setup>
 import DonationHero from '../components/Donation/DonationHero.vue';
 import DonationHero2 from '../components/Donation/DonationHero2.vue';
-import DonationBanks from '../components/Donation/DonationBanks.vue';
+// import DonationBanks from '../components/Donation/DonationBanks.vue';
 </script>
 <style scoped>
 </style>
