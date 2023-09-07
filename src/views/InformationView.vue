b29436322c8bc12ec7dffac9d174d91b9be0865e
sametbirol <sametbirol34@gmail.com>
2023-09-07 13:14:01 +0300

    remove bread crumbs from hero

--- src/views/InformationView.vue
+++ src/views/InformationView.vue
@@ -3,11 +3,11 @@
         <template v-slot:header>
             <h1 class="title">Down Sendromu Hakkında</h1>
         </template>
-        <template v-slot:router>
+        <!-- <template v-slot:router>
             <RouterLink to="/" class="inline">Anasayfa</RouterLink> / 
             <RouterLink to="/bilgi-kösesi" class="inline">Bilgi Köşesi</RouterLink> /
             <a class="disabled">Down Sendromu Hakkında</a>
-        </template>
+        </template> -->
 
     </MediaHolder>  
     <div>Bilgi Köşesi</div>
