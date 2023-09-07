a830155b5cdf950147de8f744102769123ae948d
sametbirol <sametbirol34@gmail.com>
2023-09-07 13:16:13 +0300

    media logos for contact

--- src/components/Donation/DonationContactUs.vue
+++ src/components/Donation/DonationContactUs.vue
@@ -13,11 +13,17 @@
                 </div>
                 <iframe
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d195884.30043202022!2d32.59795882005455!3d39.90352329761447!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14d347d520732db1%3A0xbdc57b0c0842b8d!2sAnkara!5e0!3m2!1str!2str!4v1694008742316!5m2!1str!2str"
-                    style="border:0;" loading="lazy"></iframe>
+                    style="border:0;" loading="lazy">
+                </iframe>
+
             </div>
+            <MediaLogo></MediaLogo>
         </div>
     </div>
 </template>
+<script setup>
+import MediaLogo from '../MediaLogo.vue'
+</script>
 <style scoped>
 .contact-form {
     width: 100%;
@@ -31,6 +37,12 @@
 
 .wrapper {
     width: 60%;
+    display: flex;
+    flex-direction: column;
+    gap: 40px;
+}
+.wrapper > div:last-child{
+    align-self: center;
 }
 
 .header {
@@ -54,7 +66,8 @@
 .inline:hover {
     background-color: transparent;
 }
-.inline{
+
+.inline {
     padding-left: 16px;
 }
 </style>
