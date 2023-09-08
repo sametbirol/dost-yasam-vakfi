4b1879682bab5ded32c0b4b13052d97a8a555176
sametbirol <sametbirol34@gmail.com>
2023-09-08 17:33:44 +0300

    activity profile made responsive

--- src/components/Activities/ActivityProfile.vue
+++ src/components/Activities/ActivityProfile.vue
@@ -1,15 +1,15 @@
 <template>
     <div class="activity">
-        <img class="activityPhoto" loading="lazy"/>
-        <div class="activityName subheader">{{ props.activityName }}</div>
-        <div class="description footer">{{ props.description }}</div>
+        <img class="activityPhoto" loading="lazy" />
+        <div class="subheader">{{ props.activityName }}</div>
+        <div class="footer">{{ props.description }}</div>
     </div>
 </template>
 <script setup>
 const props = defineProps({
-    imageURL : {
-        type : String,
-        default : "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/Solid_red.svg/512px-Solid_red.svg.png?20150316143248"
+    imageURL: {
+        type: String,
+        default: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/Solid_red.svg/512px-Solid_red.svg.png?20150316143248"
     },
     activityName: {
         type: String,
@@ -22,32 +22,63 @@ const props = defineProps({
 })
 </script>
 <style scoped>
-.activity{
+.activity {
     display: flex;
     flex-direction: column;
     justify-content: flex-start;
     align-items: flex-start;
-    width: 500px;
+    width: min-content;
 }
+
 .activityPhoto {
-    width: 500px;
-    height: 500px;
     background-color: var(--blue--darken-2);
-    border-radius: 48px;
     background-image: v-bind("props.imageURL");
     background-size: cover;
     background-position: center;
 }
-.activityName{
-    width: 80%;
-    font-family: "Elsie";
-    padding-left: 8px;
-}
-.description{
 
+.subheader {
+    font-family: "Elsie";
+    padding-left: 4px;
+}
+
+.footer {
     font-weight: 600;
-    text-align:left ;
-    padding-left: 16px;
+    text-align: left;
+    padding-left: 8px;
     font-style: unset;
+}
+
+
+@media (min-width: 1281px) {
+    .activityPhoto {
+        width: 440px;
+        height: 440px;
+        border-radius: 44px;
+    }
+}
+
+@media (min-width: 1025px) and (max-width: 1280px) {
+    .activityPhoto {
+        width: 380px;
+        height: 380px;
+        border-radius: 40px;
+    }
+}
+
+@media (min-width: 768px) and (max-width: 1024px) {
+    .activityPhoto {
+        width: 300px;
+        height: 300px;
+        border-radius: 32px;
+    }
+}
+
+@media (max-width: 767px) {
+    .activityPhoto {
+        width: 200px;
+        height: 200px;
+        border-radius: 24px;
+    }
 }
 </style>
