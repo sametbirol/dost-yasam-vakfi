11c99967874567541ffa1a010d54ad7eb327ca40
sametbirol <sametbirol34@gmail.com>
2023-09-08 17:37:38 +0300

    text fixed

--- src/layouts/Footer.vue
+++ src/layouts/Footer.vue
@@ -1,35 +1,20 @@
 <template>
-  <div id="footer">
-      <img class="icon" src="../assets/media/heart.svg" height="96" width="96">
-      <p>
-        Bu sitenin içeriği ziyaretçilerini bilgilendirmeye
-        yönelik hazırlanmış olup tıbbi teşhis, tedavi veya reçete bilgisi özelliği taşımaz.
-      </p>
-      <div>
-        ©2023 Dost Yaşam Vakfı, Tüm Hakları Saklıdır.
-      </div>
-      
+  <div class="wrapper">
+    <img src="../assets/media/heart.svg" height="96" width="96">
+    <p class="footer">
+      Bu sitenin içeriği ziyaretçilerini bilgilendirmeye
+      yönelik hazırlanmış olup tıbbi teşhis, tedavi veya reçete bilgisi özelliği taşımaz.
+    </p>
+    <div class="footer">
+      ©2023 Dost Yaşam Vakfı, Tüm Hakları Saklıdır.
+    </div>
   </div>
 </template>
 <script setup>
 </script>
 <style scoped>
-.icon {
-  color: #ffffff;
-}
-
-.icon:hover {
-  color: #000000;
-}
-
-#footer {
-  width: 100%;
+.wrapper {
   background-color: var(--blue-lighten-1);
-  display: flex;
-  align-items: center;
-  flex-direction: column;
   font-family: 'Spline Sans Mono', monospace;
-  gap:8px;
-  padding: 10px;
 }
 </style>
