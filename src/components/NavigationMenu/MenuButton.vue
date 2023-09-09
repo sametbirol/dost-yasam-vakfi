bb7e593e6dffb35ab50e1e8885ef069124a89d9b
sametbirol <sametbirol34@gmail.com>
2023-09-09 17:44:56 +0300

    menu button with transitions

--- src/components/NavigationMenu/MenuButton.vue
+++ src/components/NavigationMenu/MenuButton.vue
@@ -1,67 +1,57 @@
 <template>
-    <div class="menuButton">
+    <button class="menuButton" @click="$emit('menuToggle')">
         <div class="top"></div>
         <div class="middle"></div>
         <div class="bottom"></div>
-    </div>
+    </button>
 </template>
+<script setup>
+const handleMenu = () => {
+    console.log("hello")
+}
+</script>
 <style scoped>
-*{
+* {
     user-select: none;
 }
-.top,.bottom,.middle{
-    
-    width: 5vh;
-    height: 0.7vh;
+
+.top,
+.bottom,
+.middle {
+    transform-origin: center;
+    width: 35px;
+    height: 3px;
     border-radius: 0.3vh;
     background-color: #f5f5f5;
-    transition: transform 0.7s ease;
-    box-shadow: 0.1vh 0.3vh 0.5vh rgba(0,0,0,0.5);
+    transition: transform 0.7s ease-in-out,opacity  0.2s 0.1s ease;
+    box-shadow: 0.1vh 0.3vh 0.5vh rgba(0, 0, 0, 0.5);
 
 }
-.menuButton{
-    position: absolute;
-    top: 0;
-    right: 0;
+
+.menuButton {
+    all: unset;
     display: flex;
     flex-direction: column;
     justify-content: space-between;
     align-items: center;
-    width: 5vh;
-    height: 4vh;
-    margin-right: 4vh;
-    transform: translateY(50%);
+    width: 35px;
+    height: 21px;
 }
-.menuButton:hover{
+
+.menuButton:hover {
     cursor: pointer;
 }
-.menuButton.active > .middle{
-    display: none;
-}
 
-.menuButton.active > .top{
-    position: relative;
-    transform: translate(0,1.6vh) rotate(45deg);
-}
-.menuButton.active > .bottom{
-    position: relative;
-    transform: translate(0,-1.6vh) rotate(-45deg);
-}
-.menu.active{
-    opacity: 1;
-}
-
-.menu{
+.menuButton.active>.middle {
     opacity: 0;
-    position: absolute;
-    transition: opacity 0.5s ease;
-    top:0;
-    transform: translateY(110px);
-    width: 280px;
-    right: 2%;
-    z-index: 3;
-    background-color:#FFE8D1;
-    border-radius: 20px;
-    padding: 20px;
 }
-</style>
+
+.menuButton.active>.top {
+    position: relative;
+    transform: translate(0, 9px) rotate(45deg);
+}
+
+.menuButton.active>.bottom {
+    position: relative;
+    transform: translate(0, -9px) rotate(-45deg);
+}</style>
